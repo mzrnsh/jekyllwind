f8ebebdf9b678c7a04b4bc45b9718a42f8838b82
Kevin Gilpin <kgilpin@gmail.com>
2023-11-19 13:47:38 -0500

    Initial website copy

--- dist/tailwind.config.js
+++ dist/tailwind.config.js
@@ -8,9 +8,19 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
+    extend: {
+      colors: {
+        vintagePrimary: '#c9ae92',
+        vintagePrimaryDarker: '#b59c7a',
+        vintagePrimaryDarkerStill: '#a39470',
+        vintageSecondary: '#7e2f1a',
+        vintageBackground: '#f5f5f5',
+      },
+      fontFamily: {
+        vintage: ['Bebas Neue', 'sans-serif'],
+      },
     },
   },
-  plugins: []
+  variants: {},
+  plugins: [],
 }

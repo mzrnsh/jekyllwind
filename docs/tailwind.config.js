c5ce47437bb38f11a249b3fc026d924b2c865ff8
Kevin Gilpin <kgilpin@gmail.com>
2023-11-19 15:54:54 -0500

    site: Update colors

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         vintagePrimary: '#c9ae92',
         vintagePrimaryDarker: '#b59c7a',
         vintagePrimaryDarkerStill: '#a39470',
+        vintagePrimaryDarkest: '#8f7c66',
         vintageSecondary: '#7e2f1a',
         vintageBackground: '#f5f5f5',
       },
